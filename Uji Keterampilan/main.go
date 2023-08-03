1e67c984999685ade5ddec955ea59042b39dc4e1
ahmadktsr <141214802+ahmadktsr@users.noreply.github.com>
2023-08-03 14:15:44 +0700

    Add files via upload

---
+++ Uji Keterampilan/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"os"
+	"prakerja7/config"
+	"prakerja7/routes"
+
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	// loadEnv()
+	config.ConnectDatabase()
+	e := echo.New()
+	e = routes.InitRoute(e)
+	e.Start(getPort())
+}
+
+func getPort() string {
+	if envPort := os.Getenv("PORT"); envPort != "" {
+		return ":" + envPort
+	}
+	return ":8080"
+}
+
+func loadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		panic("Error loading .env file")
+	}
+}
